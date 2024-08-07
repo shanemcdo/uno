7e068148f18666feee58619fc8bcdef926ad5ebb
Shane McDonough <shanemcdman@gmail.com>
2024-08-07 14:43:47 -0700

    allow users to join game on main menu screen. fix #1

--- src/MainMenu.tsx
+++ src/MainMenu.tsx
@@ -7,20 +7,38 @@ import styles from './MainMenu.module.css';
 const MainMenu: Component = () => {
 	const peer = new Peer();
 	const [id, setId] = createSignal<string>('');
-	const createGameUrl = () => {
-		if(id() === '') {
-			return null;
-		}
+	const getUrl = (game_type: string, id: string ) => {
 		const url = new URL('game.html', window.location.href);
-		url.searchParams.set('type', 'create');
-		url.searchParams.set('id', id());
+		url.searchParams.set('type', game_type);
+		url.searchParams.set('id', id);
 		return url.href;
 	}
 	peer.on('open', setId);
+	let game_code_input: HTMLInputElement | undefined;
+	const [joinButtonDisabled, setJoinButtonDisabled] = createSignal(true);
 	return <div class={styles.MainMenu}>
-		<a
-			href={createGameUrl()}
-		>Create Game</a>
+		<input
+			type="button"
+			value="Create Game"
+			disabled={id() === ''}
+			onclick={() => {
+				window.location.href = getUrl('create', id());
+			}}
+		/>
+		OR
+		<input
+			placeholder="Game Code"
+			ref={game_code_input}
+			oninput={ e => setJoinButtonDisabled(e.target.value === '') }
+		/>;
+		<input
+			type="button"
+			value="Join Game"
+			disabled={joinButtonDisabled()}
+			onclick={() => {
+				window.location.href = getUrl('join', game_code_input!.value)!;
+			}}
+		/>
 	</div>;
 };
 
