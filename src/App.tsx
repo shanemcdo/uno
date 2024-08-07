9d5d24e2ac93c0cecee0d471b69afd01b3e5290a
Shane McDonough <shanemcdman@gmail.com>
2024-08-07 14:16:32 -0700

    Get basic session creating started

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import type { Component } from 'solid-js';
 
+import { Peer } from 'peerjs';
+import { createSignal, Show } from 'solid-js';
 import styles from './App.module.css';
 
 const App: Component = () => {
+	const peer = new Peer();
+	const [id, setId] = createSignal<string>('');
+	const createGameUrl = () => {
+		if(id() === '') {
+			return null;
+		}
+		const url = new URL('game.html', window.location.href);
+		url.searchParams.set('type', 'create');
+		url.searchParams.set('id', id());
+		return url.href;
+	}
+	peer.on('open', setId);
 	return <div class={styles.App}>
+		<a
+			href={createGameUrl()}
+		>Create Game</a>
 	</div>;
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
+import { Router, Route} from '@solidjs/router';
 
 import './index.css';
 import App from './App';
@@ -12,4 +13,6 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
 	);
 }
 
-render(() => <App />, root!);
+render(() => <Router>
+	<Route path="/" component={App} />
+</Router> , root!);
