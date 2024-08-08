7ea1471ae305098fdf2dd0db8e760beb052a1fb6
Shane McDonough <shanemcdman@gmail.com>
2024-08-08 07:37:03 -0700

    add Loading Screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import type { Component } from 'solid-js';
 import type { DataConnection } from 'peerjs'
 
 import { Peer } from 'peerjs';
-import { createSignal, Show } from 'solid-js';
+import { createSignal, Show, Suspense } from 'solid-js';
 import { createServer } from './server';
+import Loading from './Loading';
 import Game from './Game';
 import styles from './App.module.css';
 
@@ -75,9 +76,14 @@ const App: Component = () => {
 	});
 	return <div class={styles.App}>
 		<Show
-			when={conn() === null}
-			fallback={<Game conn={conn()!}/>}
-		> <Menu /> </Show>
+			when={id()}
+			fallback={<Loading />}
+		>
+			<Show
+				when={conn() === null}
+				fallback={<Game conn={conn()!}/>}
+			> <Menu /> </Show>
+		</Show>
 	</div>;
 };
 
