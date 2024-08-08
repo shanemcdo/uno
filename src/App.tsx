fc10384d97dcf5236a129b907110990a44addfa5
Shane McDonough <shanemcdman@gmail.com>
2024-08-07 20:20:04 -0700

    Create and starting using server

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import type { Component } from 'solid-js';
+import type { DataConnection } from 'peerjs'
 
 import { Peer } from 'peerjs';
-import { createSignal } from 'solid-js';
+import { createSignal, Show } from 'solid-js';
+import { createServer } from './server';
 import styles from './App.module.css';
 
+const peer = new Peer();
 const [id, setId] = createSignal<string>('');
+const [conn, setConn] = createSignal<DataConnection | null>(null);
+
+function connect(id: string) {
+	const conn = peer.connect(id, { reliable: true });
+	conn.on('open', () => {
+		console.log('conn connected');
+		setConn(conn);
+	});
+	conn.on('close', () => {
+		console.log('conn close');
+		setConn(null);
+	});
+	conn.on('error', err => console.log(err.message));
+}
 
 const Menu = () => {
 	let game_code_input: HTMLInputElement | undefined;
@@ -15,6 +32,7 @@ const Menu = () => {
 			value="Create Game"
 			disabled={id() === ''}
 			onclick={() => {
+				createServer(connect);
 			}}
 		/>
 		OR
@@ -28,16 +46,32 @@ const Menu = () => {
 			value="Join Game"
 			disabled={joinButtonDisabled()}
 			onclick={() => {
+				connect(game_code_input!.value);
+			}}
+		/>
+	</>;
+};
+
+const Game = () => {
+	return <>
+		{conn()!.peer}
+		<input
+			type="button"
+			value="Click me"
+			onclick={() => {
+				conn()!.send('test!');
 			}}
 		/>
 	</>;
 };
 
 const App: Component = () => {
-	const peer = new Peer();
 	peer.on('open', setId);
 	return <div class={styles.App}>
-		<Menu />;
+		<Show
+			when={conn() === null}
+			fallback={<Game/>}
+		> <Menu /> </Show>
 	</div>;
 };
 
---
+++ src/server.tsx
@@ -0,0 +1,15 @@
+import { Peer } from 'peerjs';
+
+export function createServer(callback: (id: string) => void): Peer {
+	const peer = new Peer();
+	peer.on('open', callback);
+	peer.on('error', err => console.log(err.message));
+	peer.on('close', () => console.log('server close'));
+	peer.on('connection', conn => {
+		console.log(conn.peer);
+		conn.on('data', data => {
+			console.log(data);
+		})
+	});
+	return peer;
+};
