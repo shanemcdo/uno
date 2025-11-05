07d515d869ce90e95052861b4c1e1244b910c8f1
Shane McDonough <shanemcdman@gmail.com>
2025-11-04 23:02:01 -0800

    Give a player a hand when they join the game
    Closes #17

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,49 @@
 import type { DataConnection } from 'peerjs';
-import type{ ClientData, MessageBroadcast, NameValidation } from './types';
+import type { ClientData, MessageBroadcast, NameValidation } from './types';
+import type { Card } from './deck';
 
 import { ServerType, ClientType } from './types';
 import { Peer } from 'peerjs';
 import { deck } from './deck';
+import deepClone from './deepClone';
+import rand from './rand';
 
 type PlayerData = {
 	conn: DataConnection
 	name: string,
+	hand: Card[],
 };
 
+const STARTING_HAND_SIZE = 7;
+
 const playerData: Record<string, PlayerData> = {};
+let currentDeck = shuffle(deepClone(deck));
+
+// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+function shuffle<T>(arr: T[]): T[] {
+	for(let i = arr.length - 1; i >= 1; i--) {
+		const j = rand(0, i + 1);
+		if(i === j) continue;
+		[ arr[i], arr[j] ] = [ arr[j], arr[i] ];
+	}
+	return arr;
+}
+
+function drawCard(): Card {
+	if(currentDeck.length < 1) {
+		currentDeck = shuffle(deepClone(deck));
+	}
+	const card = currentDeck.pop();
+	return card!;
+}
+
+function drawCards(count: number): Card[] {
+	const result = [];
+	for(let i = 0; i < count; i++) {
+		result.push(drawCard());
+	}
+	return result;
+}
 
 function nameExists(name: string): boolean {
 	for(const player of Object.values(playerData)) {
@@ -38,7 +71,10 @@ export function createServer(callback: (id: string) => void): Peer {
 					playerData[conn.peer] = {
 						conn,
 						name: d.name,
+						hand: drawCards(7),
 					};
+					console.log(playerData);
+					console.log(currentDeck);
 				}
 				conn.send({
 					type: ServerType.Name,
