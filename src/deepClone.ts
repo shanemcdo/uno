5121c696cb7fc1e9097fd91aa591bdaf77398650
Shane McDonough <shanemcdman@gmail.com>
2025-11-04 23:00:21 -0800

    fix deepclone so copies aren't readonly

--- src/deepClone.ts
+++ src/deepClone.ts
@@ -1,6 +1,7 @@
 // https://www.geeksforgeeks.org/typescript/how-to-deep-clone-an-object-preserve-its-type-with-typescript/
+// https://stackoverflow.com/questions/42999983/typescript-removing-readonly-modifier#43001581
 type DeepClone<T> = T extends object ?
-	{ [K in keyof T]: DeepClone<T[K]> } :
+	{ -readonly [K in keyof T]: DeepClone<T[K]> } :
 	T;
 export default function deepClone<T>(obj: T): DeepClone<T> {
 	return JSON.parse(JSON.stringify(obj));
