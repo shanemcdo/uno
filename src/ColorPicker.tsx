f92d1398f9443db5644aabc577fbee1afbed0798
Shane McDonough <shanemcdman@gmail.com>
2025-11-13 21:17:12 -0800

    update color picker css and use data-background mixin

--- src/ColorPicker.tsx
+++ src/ColorPicker.tsx
@@ -3,17 +3,27 @@ import type { Component } from 'solid-js'
 import { Color } from './deck';
 import styles from './ColorPicker.module.scss'
 
+
 type Props = {
 	callback: (color: Color) => void,
 };
 
+type ChoiceProps = Props & {
+	color: Color,
+};
+
+const Choice: Component<ChoiceProps> = props =>
+	<div data-color={props.color} class={styles.choice} onclick={() => props.callback(props.color)} />;
+
 const ColorPicker: Component<Props> = props => {
 	return <div class={styles.color_picker}>
-		<h2>Color Picker</h2>
-		<p onclick={() => props.callback(Color.Red)}>Red</p>
-		<p onclick={() => props.callback(Color.Blue)}>Blue</p>
-		<p onclick={() => props.callback(Color.Green)}>Green</p>
-		<p onclick={() => props.callback(Color.Yellow)}>Yellow</p>
+		<h2>Color Picker:</h2>
+		<div class={styles.choices}>
+			<Choice callback={props.callback} color={Color.Red} />
+			<Choice callback={props.callback} color={Color.Blue} />
+			<Choice callback={props.callback} color={Color.Green} />
+			<Choice callback={props.callback} color={Color.Yellow} />
+		</div>
 	</div>;
 }
 
